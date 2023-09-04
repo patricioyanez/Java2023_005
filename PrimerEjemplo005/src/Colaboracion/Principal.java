2937ce6a26e5a5a79994cebaa99e513d15e7cb62
patricioyanez <66755362+patricioyanez@users.noreply.github.com>
2023-09-04 16:35:57 -0300

    clase 5

--- PrimerEjemplo005/src/Colaboracion/Principal.java
+++ PrimerEjemplo005/src/Colaboracion/Principal.java
@@ -1,5 +1,7 @@
 package Colaboracion;
 
+import java.util.Scanner;
+
 public class Principal {
 
     public static void main(String[] args) {
@@ -19,5 +21,29 @@ public class Principal {
         
         System.out.println("\nSegundos faltantes: " + 
                 streaming.calcularSegundosFaltantes());
+        
+                
+        // ejercicio 2:
+        // Crear otra canción y reproducirla.
+        // solicitar los datos por la consola. 
+        // todos los datos son obligatorios.
+        
+        Cancion cancion2 = new Cancion();
+        
+        Scanner leerTeclado = new Scanner(System.in);
+        System.out.print("Nombre    : ");
+        cancion2.setNombre(leerTeclado.next());
+        System.out.print("Banda     : ");
+        cancion2.setArtista(leerTeclado.next());
+        System.out.print("Album     : ");
+        cancion2.setAlbum(leerTeclado.next());
+        System.out.print("Género    : ");
+        cancion2.setGenero(leerTeclado.next());
+        System.out.print("Duración  : ");
+        cancion2.setDuracion(leerTeclado.nextInt());
+        
+        streaming.setCancion(cancion2);
+        streaming.setSegundosReproducidos(50);
+        System.out.println(streaming.imprimir());
     }
 }
