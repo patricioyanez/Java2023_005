398ed4ad5e4d41e0739946460298c6369eba8f11
patricioyanez <66755362+patricioyanez@users.noreply.github.com>
2023-09-26 09:56:28 -0300

    ejercicio

---
+++ Scootin005/src/Empresa.java
@@ -0,0 +1,14 @@
+
+import java.util.ArrayList;
+
+
+public class Empresa {
+    // otra forma de crear la colección sin usar constructor
+    //ArrayList<Empleado> empleados = new ArrayList<Empleado>();
+    ArrayList<Empleado> empleados;
+    
+    public Empresa()
+    {
+        empleados = new ArrayList<Empleado>();
+    }
+}
