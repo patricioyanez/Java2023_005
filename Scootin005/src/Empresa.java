c06f20e89443983ec4c65db2031951318b99d080
patricioyanez <66755362+patricioyanez@users.noreply.github.com>
2023-10-02 09:14:48 -0300

    update

--- Scootin005/src/Empresa.java
+++ Scootin005/src/Empresa.java
@@ -11,4 +11,35 @@ public class Empresa {
     {
         empleados = new ArrayList<Empleado>();
     }
+    
+    // CRUD: create, read, update y delete
+    public boolean agregar(Empleado empleado)
+    {
+        // ejercicio 1: evitar que el rut se repita???
+        return empleados.add(empleado);
+    }
+    public boolean buscarEmpleado(String rut)
+    {
+        for (Empleado empleado : empleados) {
+            if(empleado.getRut().equals(rut))
+                return true;
+        }
+        return false;
+    }
+    
+    public void listarEmpleado()
+    {
+        for (Empleado empleado : empleados) {
+            System.out.println(empleado.imprimir());
+        }
+    }
+    
+    public boolean eliminarEmpleado(String rut)
+    {
+        for (Empleado empleado : empleados) {
+            if(empleado.getRut().equals(rut))
+                return empleados.remove(empleado);
+        }
+        return false;
+    }
 }
