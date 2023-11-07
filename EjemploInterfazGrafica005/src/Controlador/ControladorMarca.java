ebb18742ec4ae4390466095a3e28de484d47d905
patricioyanez <66755362+patricioyanez@users.noreply.github.com>
2023-11-07 09:10:21 -0300

    Update ControladorMarca.java

--- EjemploInterfazGrafica005/src/Controlador/ControladorMarca.java
+++ EjemploInterfazGrafica005/src/Controlador/ControladorMarca.java
@@ -4,6 +4,7 @@ import BD.Conexion;
 import Modelo.Marca;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
 
@@ -34,17 +35,82 @@ public class ControladorMarca {
     }
     public boolean actualizar(Marca marca)
     {
-        return true;
+        try{
+            Conexion con = new Conexion();
+            Connection cx = con.obtenerConexion();
+
+            String sql = "UPDATE MARCA SET NOMBRE = ?, HABILITADO = ? WHERE ID = ?";
+            PreparedStatement st;
+
+            st = cx.prepareStatement(sql);
+            st.setString(1, marca.getNombre());
+            st.setBoolean(2, marca.isHabilitado());
+            st.setInt(3, marca.getId());
+            
+            st.executeUpdate();
+            st.close(); // cierra declaración
+            cx.close(); // cierra conexión a la BDD
+            return true;
+        } catch (SQLException ex)
+        {
+            System.out.println("Error: " + ex.getMessage());
+        }
+        return false;
     }
-    public boolean eliminar(int marca)
+    public boolean eliminar(int id)
     {
-        return true;
-    }
-    public ArrayList<Marca> buscarTodos()
-    {
-        return null;
+        try{
+            Conexion con = new Conexion();
+            Connection cx = con.obtenerConexion();
+
+            String sql = "DELETE FROM MARCA WHERE ID = ?";
+            PreparedStatement st;
+
+            st = cx.prepareStatement(sql);
+            st.setInt(1, id);
+            
+            st.executeUpdate();
+            st.close(); // cierra declaración
+            cx.close(); // cierra conexión a la BDD
+            return true;
+        } catch (SQLException ex)
+        {
+            System.out.println("Error: " + ex.getMessage());
+        }
+        return false;
     }
     public Marca buscarPorId(int id)
+    {
+        try{
+            Conexion con = new Conexion();
+            Connection cx = con.obtenerConexion();
+
+            String sql = "SELECT ID, NOMBRE, HABILITADO FROM MARCA WHERE ID=?";
+            PreparedStatement st;
+
+            st = cx.prepareStatement(sql);
+            st.setInt(1, id);
+            
+            ResultSet rs = st.executeQuery();
+            
+            if(rs.next())
+            {
+                Marca marca = new Marca();
+                marca.setId(rs.getInt("ID"));
+                marca.setNombre(rs.getString("NOMBRE"));
+                marca.setHabilitado(rs.getBoolean("HABILITADO"));
+                return marca;
+            }
+            st.close(); // cierra declaración
+            cx.close(); // cierra conexión a la BDD
+            return null;
+        } catch (SQLException ex)
+        {
+            System.out.println("Error: " + ex.getMessage());
+        }
+        return null;
+    }
+    public ArrayList<Marca> buscarTodos()
     {
         return null;
     }
